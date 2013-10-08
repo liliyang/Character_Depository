32288b6902a219a3c4306a9a851c3f53a158c798
Lili Yang <liliyang369@gmail.com>
2013-10-08 15:53:31 -0400

    A few more character view and form changes.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ActiveRecord::Base
   
-  before_save :check_candidate
+  before_save :check_candidate, :check_rider
   
   has_many :pets
   has_one :dragon, dependent: :destroy, foreign_key: :rider_id
@@ -10,6 +10,12 @@ class Character < ActiveRecord::Base
   def check_candidate
     if age > 23 
       self.candidate = false
+    end
+  end
+  
+  def check_rider
+    if dragon
+      self.rider = true
     end
   end
 end
