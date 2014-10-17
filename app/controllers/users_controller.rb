65b728dda5ceb059aa68b68e8fe2c2ee9735b08b
Lili Yang <yanglili369@gmail.com>
2014-10-16 22:36:56 -0700

    temp password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:show, :edit, :update, :destroy, :set_password]
-  before_action :signed_in_user, only: [:index, :show, :edit, :update, :destroy, :set_password]
-  before_action :correct_user, only: [:edit, :update, :destroy] #, :set_password]
+  # before_action :signed_in_user, only: [:index, :show, :edit, :update, :destroy, :set_password]
+  # before_action :correct_user, only: [:edit, :update, :destroy, :set_password]
   before_action :admin_user, only: [:index]
   before_action :check_update_password, only: [:update]
 
