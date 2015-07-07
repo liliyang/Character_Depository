7aaa8100a6c6c8ece53ffd890ae4d4d9b0e8e968
Lili Yang <yanglili369@gmail.com>
2015-07-06 18:11:28 -0700

    updated character.rb

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,8 +6,8 @@ class Character < ActiveRecord::Base
   has_one :dragon, dependent: :destroy, foreign_key: :rider_id
   
   has_many :relationships, dependent: :destroy
-  has_many :relatives, through: :relationships, dependent: :destroy
-  has_many :inverse_relationships, :class_name => "Relationship", foreign_key: :relative_id
+  has_many :relatives, through: :relationships
+  has_many :inverse_relationships, :class_name => "Relationship", foreign_key: :relative_id, dependent: :destroy
   has_many :inverse_relatives, through: :inverse_relationships, source: :character
   
   validates :name, presence: true, uniqueness: true
